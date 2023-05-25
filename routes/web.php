09f7ac35340fead589bcea4f021e774cbb9c66d2
agwadan <agwa.dan@gmail.com>
2023-05-25 14:12:11 +0300

    Basic routing intro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return response('<h2>Hello World</h2>', 404)
+        ->header('Content-type', 'text/plain')
+        ->header('foo', 'bar'); //------------------------ Custom header
+});
+
+Route::get('/post/{id}', function ($id) {
+
+    //dd($id);
+    //ddd($id);
+
+    return response('Post ' . $id);
+})->where('id', '[0-9]+'); //----------------------------- Limit the id to be numbers
+
+Route::get('/search', function (Request $request) {
+    //dd($request->name . ' lives in ' . $request->city);
+    return ($request->name . ' lives in ' . $request->city);
 });
