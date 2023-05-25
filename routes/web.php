b94131e1bebd04b256160bf5ddec93c18834c2a8
agwadan <agwa.dan@gmail.com>
2023-05-25 14:45:22 +0300

    Simple parsing of data

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listing',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing one',
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing two',
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Listing three',
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            ],
+        ]
+    ]);
 });
 
-Route::get('/hello', function () {
+/* Route::get('/hello', function () {
     return response('<h2>Hello World</h2>', 404)
         ->header('Content-type', 'text/plain')
-        ->header('foo', 'bar'); //------------------------ Custom header
+        ->header('foo', 'bar'); /------------------------ Custom header
 });
 
 Route::get('/post/{id}', function ($id) {
@@ -30,9 +49,9 @@ Route::get('/post/{id}', function ($id) {
     //ddd($id);
 
     return response('Post ' . $id);
-})->where('id', '[0-9]+'); //----------------------------- Limit the id to be numbers
+})->where('id', '[0-9]+'); /----------------------------- Limit the id to be numbers
 
 Route::get('/search', function (Request $request) {
     //dd($request->name . ' lives in ' . $request->city);
     return ($request->name . ' lives in ' . $request->city);
-});
+}); */
