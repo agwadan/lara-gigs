a4be2ae182d150f373d883a83e142a0fbad9e893
agwadan <agwa.dan@gmail.com>
2023-05-29 18:00:15 +0300

    Create layouts

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,8 @@
-<h1>{{$listing['title']}}</h1>
-<p>{{$listing['description']}}</p>
+@extends('layout')
+
+@section('content')
+
+  <h1>{{$listing['title']}}</h1>
+  <p>{{$listing['description']}}</p>
+
+@endsection
