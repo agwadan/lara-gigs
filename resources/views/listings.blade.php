56d8de0e361eea00960a32eba932da611514d2f5
agwadan <agwa.dan@gmail.com>
2023-05-29 18:18:50 +0300

    Style Listings page

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,8 +1,10 @@
 @extends('layout')
 
 @section('content')
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+@include('partials._hero')
+@include('partials._search')
 
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
   @unless(count($listings) == 0)
     @foreach ($listings as $listing)
     <div class="bg-gray-50 border border-gray-200 rounded p-6">
@@ -14,7 +16,7 @@
           />
           <div>
               <h3 class="text-2xl">
-                  <a href="show.html">{{$listing->title}}</a>
+                  <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
               </h3>
               <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
               <ul class="flex">
