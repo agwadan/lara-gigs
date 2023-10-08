c478d35a19c12068b355861966e703473da7b094
agwadan <agwa.dan@gmail.com>
2023-10-08 10:59:22 +0300

    Fix font for location on listings

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -14,7 +14,8 @@
           <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
          <x-listing-tags :tagsCsv="$listing->tags"/>
           <div class="text-lg mt-4">
-              <i class="fa-solid fa-location-dot">{{$listing->location}}</i> 
+              <i class="fa-solid fa-location-dot"></i> 
+              {{$listing->location}}
           </div>
       </div>
   </div>
